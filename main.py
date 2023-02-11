0a60fdbacc0097fb3de5d5f052c406720431c4ae
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2023-02-11 21:39:16 +0330

    Some basic python operations has done.

--- main.py
+++ main.py
@@ -1,2 +1,41 @@
 
 print("Hello Python")
+
+num1 = 10
+num2 = 25
+
+sum = num1 + num2;
+print("The sum of two mentioned numbers is: ", sum);
+
+number1 = 7
+number2 = 3
+
+result = number1/number2
+print("The result is: ", round(result, 2))
+
+str1 = "Hello Ahmad! how are you?";
+print(str1)
+print(str1[1])
+
+str1 = "Afghanistan"
+pro_num = 34
+
+print(str1 + " has "+ str(pro_num) +" provinces.")
+
+
+M_class = ["Faisal", "Ahmad", "Ali", "Farhad", "Fahim"]
+print("'M_class' has "+ str(len(M_class)) + " items.")
+print(M_class)
+print(M_class[2:])
+M_class.append("Fawad")
+print(M_class)
+M_class.insert(2, "Zobair")
+print(M_class)
+print("Fawad" in M_class)
+
+
+F_class = ["Fatemah", "Safa", "Layla"]
+
+total_stu = M_class + F_class
+print(total_stu)
+
