39ca2e9519fb34fc8687803cbc427998c7c272c7
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2023-02-12 17:20:30 +0330

    Python: Math builton python module has implemented.

---
+++ Pythoh Modules/mathModule.py
@@ -0,0 +1,11 @@
+# Modules is a way to reuse a code written by someone.
+import math
+print("The square of 9 is: ", math.sqrt(9))
+print("2 power of 3 is equal to: ", math.pow(2, 3))
+print("Price of Pi is equal to: ", math.pi)
+print("Log of 100 base on 10 is equal to: ", math.log10(100))
+print("Log of 1000 base on 10 is equal to: ", math.log10(1000))
+
+
+# To check all functions in math module:
+print(dir(math))
