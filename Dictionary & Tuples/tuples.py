efa682e6428106bc983ae23eeddd6bf974a955b8
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2023-02-12 10:32:17 +0330

    Python: Tuples concepts is cleared.

---
+++ Dictionary & Tuples/tuples.py
@@ -0,0 +1,18 @@
+# Tuples is a list of values grouped together.
+x_y = (12, 34)
+print(x_y)
+# To display separately:
+print(x_y[0])
+print(x_y[1])
+
+# The most difference between python list and python tuples is: list is Homogenous mean all the values show similar
+# meaning. But tuples are Heterogeneous mean all the values show different meanings.
+# Next, we can change the specific value of python list, but we can't change the value of tuple.
+
+address = ("2nd Street, ", "Bagh Bala, ", "Kabul, ", "Afghanistan")
+print(address)
+
+print(address[0])
+print(address[1])
+print(address[2])
+print(address[3])
