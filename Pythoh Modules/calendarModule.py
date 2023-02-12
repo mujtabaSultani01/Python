6cbfee2b02414decffae897ddd107530ff875ed5
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2023-02-12 17:21:03 +0330

    Python: calendar builton python module has implemented.

---
+++ Pythoh Modules/calendarModule.py
@@ -0,0 +1,8 @@
+import calendar
+print(dir(calendar))
+print(calendar.month(1997, 4))
+print(calendar.isleap(2020))
+
+# To find all available modules in python, you can google Python 3.9.12 modules list. This shows you the modules which
+# installed in you your python. We also have 3rd party modules, for using them you should install them separately.
+# For your private needs, you can write your own modules. (check => calculate.py & myProgram.py)
