2510e890d677a5d0de9f6455a8bd6be9fc8c8921
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2023-02-12 08:19:54 +0330

    Simple modification is made in if_else.py file.

--- if_else.py
+++ if_else.py
@@ -1,5 +1,7 @@
 
 # Debugging is the process to execute code line by line and do analysis on what's going on at each step.
+
+#  Simple if_else condition.
 number = input("Enter a number: ")
 number = int(number)
 
@@ -9,7 +11,7 @@ if number%2 == 0:
 else:
     print("You entered odd number.")
 
-
+# Finding students using if-else class.
 Male_class = ["Faisal", "Fawad", "Fayez", "Faizan"]
 Female_class = ["Marwa", "Safa", "Khadija", "Sana"]
 
@@ -21,3 +23,14 @@ elif stu_name in Female_class:
     print("She is founded in Femal class.")
 else:
     print("He is not our student.")
+
+# Simple example of finding keys through if_else condition...
+
+# key = "garage"
+key = input("Enter key location: ")
+locations = ["bedroom", "chair", "garage", "Cabinet"]
+
+if key in locations:
+    print("Key found at ", key, " location.")
+else:
+    print("Key is not found!!!")
