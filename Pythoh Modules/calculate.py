e79b1b5a3a6f597b5bdfff5e800f6b35325abb1c
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2023-02-12 17:22:07 +0330

    Python: user define module sample which has two functions. return the area of tringle and square.

---
+++ Pythoh Modules/calculate.py
@@ -0,0 +1,7 @@
+# This file works as module for other files...
+def calculate_tringle_area(base, height):
+    return 1/2 * (base * height)
+
+def calculate_square_eara(length):
+    return length * length
+
