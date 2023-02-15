23922d9f05f9c3dcda805e4aab744633c74e6aa8
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2023-02-15 08:07:48 +0330

    Python: Multiprocessing (sharing data between processes using value) concept is cleared through an example.

--- Multiprocessing/data_share.py
+++ Multiprocessing/data_share.py
@@ -2,7 +2,7 @@
 The key difference between Multithreading and Multiprocessing is, in Multithreading you will see the result being fill
 outside the function but in Multiprocessing you won't see the result outside the function.
 So let's solve the problem and share data between two processes. For that we use Shared Memory concept.
-There are couple of ways using shared memory: 1) value 2) array
+There are two ways using shared memory: 1) value 2) array
 Let's first see the array and then value:
 """
 
@@ -15,7 +15,7 @@ def calculate_square(numbers, res):
 
 
 if __name__ == "__main__":
-    array = [2,3,4,5]
+    array = [2, 3, 4, 5]
     result = multiprocessing.Array('i', 4)
     P1 = multiprocessing.Process(target=calculate_square, args=(array, result))
 
@@ -23,4 +23,26 @@ if __name__ == "__main__":
     P1.join()
 
     print("Result: " + str(result[:]))
+    print("Let's done it...")
+
+# Let's see data sharing concept using vlue:
+
+
+def calculate_square1(numbers, resu, va):
+    va.value = 3.10
+    for idx, n in enumerate(numbers):
+        resu[idx] = n*n
+
+
+if __name__ == "__main__":
+    array = [2, 3, 4, 5]
+    result = multiprocessing.Array('i', 4)
+    v = multiprocessing.Value('d', 0.0)
+    P1 = multiprocessing.Process(target=calculate_square, args=(array, result))
+
+    P1.start()
+    P1.join()
+
+    print("Result: " + str(result[:]))
+    print("Value: " + str(v.value))
     print("Let's done it...")
